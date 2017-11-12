3edda2aac4bfc628f3506d52afec49a7709f5998
Anddrullu <andreas@t-jensen.dk>
2017-11-12 01:13:16 +0100

    java test

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+
+import java.net.*;    
+
+public class Problem1 { 
+
+    public static void main(String[] args){ 
+        try { 
+            InetAddress address = InetAddress.getLocalHost(); 
+            System.out.println(address); 
+        } 
+        catch (UnknownHostException uhEx) { 
+        	System.out.println( "Could not find local address!");
+        }
+    } 
+} 
